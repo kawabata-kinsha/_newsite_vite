3887bd50c2a9a574b4ad5dea524919bb30c1617e
Ryo Kawabata <kawabata@kinsha.co.jp>
2023-09-14 14:41:23 +0900

    PHPエラー表示off

--- www/index.php
+++ www/index.php
@@ -1,5 +1,4 @@
 <?php
-//ini_set('display_errors', "On");
 
 //include(dirname(__FILE__)."/php/_config.php");
 include(substr(dirname(__FILE__), 0, strpos(dirname(__FILE__), '/www')) . '/www/php/_config.php');
